5711ff9fbcdf8a375d7be20af39a6d78e92f69c3
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-06-29 14:58:46 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 import logging
 import telebot
 from dotenv import load_dotenv
+from main import index_logs
+from fastapi import Request
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
@@ -19,14 +21,19 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, f"Howdy, how are you doing? This is your chat ID: {message.chat.id}")
 
+@bot.message_handler(commands=['logs'])
+def send_welcome(message):
+    message_logs = index_logs(Request)
+    print(message)
+    bot.reply_to(message, f"Howdy, how are you doing? This is your log: {message_logs}")
 
-def send_logs():
+def send_logs(logs = index_logs(Request)):
     message = "This is a log"
     bot.send_message(1047727961, message)
 
-@bot.message_handler(func=send_logs())
+@bot.message_handler(func=index_logs(Request))
 def echo_all(message):
-    message = "This is a log"
+    message = index_logs(Request)
     bot.send_message(1047727961, message)
 
 bot.infinity_polling()
